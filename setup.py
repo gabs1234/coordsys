75d2cea68c5c093dae2ea8d7db7a7a74c7d3f1d7
gabs1234 <gabslefloch@gmail.com>
2025-03-10 16:55:39 +0100

    Modified project name

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="quaternion_frame",
+    name="coordsys",
     version="0.1.0",
     packages=find_packages(),
-    description="A package for quaternion-based coordinate frame operations",
-    author="Author",
+    description="A Python package for quaternion-based coordinate frame operations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Gabriel Lefloch",
+    author_email="gabriel.lefloch@kit.edu",
+    url="https://github.com/gabs1234/coordsys",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
     install_requires=[
         "numpy",
         "pyvista",
